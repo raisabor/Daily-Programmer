c0305bbfdb532373e5b6a2c1bc1e955cd64070aa
Rich Aisabor <raisabor@smu.edu>
2019-06-23 13:27:14 -0500

    An Angram class that checks if two strings are valid anagrams and another class that adds one to the last index in an array

---
+++ Anagram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Anagram
+{
+    public bool IsAnagram(string s, string t)
+    {
+
+        char[] arr = s.ToCharArray();
+        char[] marr = t.ToCharArray();
+
+
+
+        int number = 0;
+
+
+        for (int i = 0; i < arr.Length; i++)
+
+        {
+
+
+            number += (int)arr[i];
+
+        }
+
+
+        int number2 = 0;
+
+
+        for (int i = 0; i < marr.Length; i++)
+
+
+        {
+
+            number2 += (int)marr[i];
+
+
+        }
+
+
+        if (number == number2)
+
+            return true;
+
+        else
+
+            return false;
+
+
+    }
+}
---
+++ Plus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Plus
+{
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (i == digits.Length - 1)
+                {
+                    digits[i]++;
+                }
+
+
+            }
+
+            return digits;
+
+
+        }
+   
+}
